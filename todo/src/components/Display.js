201e62c05764bfbf5a4dd493c03e23587054bf46
Gauss15 <95584122+Gauss15@users.noreply.github.com>
2022-06-19 12:37:09 -0500

    Add files via upload

---
+++ todo/src/components/Display.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Display = (props) => {
+
+  const { todoList, setTodoList } = props;
+
+
+  const handleCompleted = (todo) => {
+
+    todo.markedDelete = !todo.markedDelete;
+    setTodoList([...todoList]);
+  };
+
+  const styled = (markedDelete) => {
+    if (markedDelete === true) {
+      return "completed";
+    } else {
+      return "notCompleted";
+    }
+  };
+
+  const deleteButton = (idFromBelow) => {
+    setTodoList(
+      todoList.filter((todo, index) => {
+        return todo.id !== idFromBelow;
+      })
+    );
+  };
+  return (
+    <div>
+      {todoList.map((todo, index) => (
+        <div className={styled(todo.markedDelete)} key={index}>
+          <p>{todo.content}</p>
+          <input type="checkbox" onChange={(e) => handleCompleted(todo)} />
+          <button onClick={() => deleteButton(todo.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  ); 
+};
+
+export default Display
